4dc5b771aa3676f097e644bebab1371121bcbb91
mgonzalz <mgonzgarc@myuax.com>
2022-11-22 00:51:41 +0100

    c3

--- black_jack.py
+++ black_jack.py
@@ -17,10 +17,24 @@ print("***"*5, "\n")
 print ("BLACK JACK", "\n")
 print("***"*5, "\n")
 #VALOR DE CADA CARTA
-print("Cartas disponibles: {}".format(cartas.keys()))
+print("Cartas disponibles: {}".format(list(cartas.keys())))
 print("Valor correspondiente de las cartas: {}".format(list(cartas.values())))
 #LISTA CON PUNTOS CARTAS
 print ("1. Valor de cada carta:")
 for clave, valor in cartas.items():
     print(clave, ": ", valor)
 #JUGADOR DOS CARTAS AL AZAR
+print("Bienvenido al juego de Black Jack")
+import random
+carta_jugador = random.choice(list(cartas.keys()))
+puntuacion = int(cartas[carta_jugador])
+print("Ha seleccionado la siguiente carta ", carta_jugador, " teniendo un valor de: ", puntuacion)
+input(print("¿Desea seleccionar otra carta?"))
+if input == "si" or input == "Si":
+    carta_jugador_2 = random.choice(list(cartas.keys()))
+    puntuacion+=cartas[carta_jugador_2]
+    print("Ha seleccionado las siguientes cartas ", carta_jugador, "y", carta_jugador_2," teniendo un valor de: ", puntuacion)
+elif input == "no" or input == "No":
+    print("Ha seleccionado la siguiente carta ", carta_jugador, " teniendo un valor de: ", puntuacion)
+else:
+    print("Opción no válida")
