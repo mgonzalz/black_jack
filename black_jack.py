3d531c22f03b6b60c0c804daf0ba89fdaa89d359
mgonzalz <mgonzgarc@myuax.com>
2022-11-22 01:15:00 +0100

    c4

--- black_jack.py
+++ black_jack.py
@@ -23,18 +23,31 @@ print("Valor correspondiente de las cartas: {}".format(list(cartas.values())))
 print ("1. Valor de cada carta:")
 for clave, valor in cartas.items():
     print(clave, ": ", valor)
+#ELECCIÓN CARTA DE LA BANCA
+def banca_azar():
+    import random
+    carta_banca = random.choice(list(cartas.keys()))
+    puntuacion_banca = int(cartas[carta_banca])
+    return puntuacion_banca, carta_banca
+puntuacion_banca, carta_banca = banca_azar()
+puntuacion_banca2, carta_banca2 = banca_azar()
 #JUGADOR DOS CARTAS AL AZAR
 print("Bienvenido al juego de Black Jack")
 import random
 carta_jugador = random.choice(list(cartas.keys()))
 puntuacion = int(cartas[carta_jugador])
 print("Ha seleccionado la siguiente carta ", carta_jugador, " teniendo un valor de: ", puntuacion)
-input(print("¿Desea seleccionar otra carta?"))
-if input == "si" or input == "Si":
+p = input("¿Desea seleccionar otra carta?")
+print(p)
+if p == "si" or p == "Si":
     carta_jugador_2 = random.choice(list(cartas.keys()))
     puntuacion+=cartas[carta_jugador_2]
-    print("Ha seleccionado las siguientes cartas ", carta_jugador, "y", carta_jugador_2," teniendo un valor de: ", puntuacion)
-elif input == "no" or input == "No":
+    print("Ha seleccionado las siguientes cartas ", carta_jugador, "y",carta_jugador_2," teniendo un valor de: ", puntuacion)
+    banca_azar()
+    print("La banca ha seleccionado las siguientes cartas ", carta_banca, "y", carta_banca2, " teniendo un valor de: ", puntuacion_banca+puntuacion_banca2)
+elif p == "no" or p == "No":
     print("Ha seleccionado la siguiente carta ", carta_jugador, " teniendo un valor de: ", puntuacion)
+    banca_azar()
+    print("La banca ha seleccionado la siguiente carta ", carta_banca, " teniendo un valor de: ", puntuacion_banca)
 else:
     print("Opción no válida")
